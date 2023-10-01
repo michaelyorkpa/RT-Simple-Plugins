0727436f939d825f56474c40ced02894374b0c64
Michael York <36156735+michaelyorkpa@users.noreply.github.com>
2023-10-01 01:51:40 -0400

    Functionality added & 1 Bug Squashed; See Readme
    Signed-off-by: Michael York <36156735+michaelyorkpa@users.noreply.github.com>

--- rt-simple-plugins/README.md
+++ rt-simple-plugins/README.md
@@ -6,11 +6,14 @@ This is a combination of plugins that will add funcionality to WordPress in a wa
 ### Current Features
 * Last Updated Date
   * Adds last updated date to your posts above the content
+  * Gives the option to show the originally published date too
 
 ### Future Features
 * Last Updated Date
   * Selecting where the date goes on the page
-  * Including the originally posted date in this setting, giving it a "news" feel
+  * Custom CSS for original publishing date
+  * Custom text before the originally published date
+  * Custom date formatting
 * Easy 301 Redirects
   * A simple way, lightweight way to create 301 redirects for any URL 
 
@@ -18,10 +21,13 @@ This is a combination of plugins that will add funcionality to WordPress in a wa
 * 0.15 
   * Updated plugin name and variables to reflect this change
   * Moved plugin settings to their own page, rather than in the settings menu
-    * Updated dashicon
-  * 
+  * Updated dashicon
+  * Added option to include the originally published date
+  * Added option to customize the text preceeding the last updated date
+  * Added second column for admin panel; still need to fix the layout, it's dual posting all the fields
+  * Fixed the issues with registering and deregistering the options when the plugin is activated/deactivated
 * 0.12-0.14
-  * A bunch of minor changes and funcationality improvements including adding a hook to deregister and delete the options from the database when deactivating; created this as a user selectable toggle
+  * A bunch of minor changes and functionality improvements including adding a hook to deregister and delete the options from the database when deactivating; created this as a user selectable toggle
 * 0.11 
   * Added adjustable threshold setting in admin menu
   * Allows user to select number of hours difference between creation and modification date
--- rt-simple-plugins/rt-simple.php
+++ rt-simple-plugins/rt-simple.php
@@ -22,7 +22,11 @@ function initialize_rt_simple_plugin_options() {
   $default_options = array(
       'threshold_hours' => 12,
       'delete_options_enabled' => 0,
-      'custom_css' => ''
+      'custom_css' => '',
+      'display_on_pages' => 1,
+      'display_on_posts' => 1,
+      'custom_text' => 'Last Updated on ', // Add default custom text option
+      'display_original_publish_date' => 1
       // Add more default options if needed
   );
 
@@ -36,30 +40,31 @@ function initialize_rt_simple_plugin_options() {
 function rt_simple_plugin_activation() {
   // Initialize default plugin options
   initialize_rt_simple_plugin_options();
-
   // Additional activation tasks can go here if needed
-  register_activation_hook(__FILE__, 'rt_simple_plugin_activation');
 }
+
+register_activation_hook(__FILE__, 'rt_simple_plugin_activation');
 
 
 //Registers the settings and fields
 function rt_simple_date_settings_init() {
 register_setting('rt_simple_options', 'rt_simple_date_display_options');
 
-add_settings_section('rt_simple_date_main_section', 'Display Options', '', 'rt_simple_options');
+add_settings_section('rt_simple_date_main_section', 'Date Display Options', '', 'rt_simple_options');
 add_settings_field('threshold_hours', 'Threshold (in hours)', 'threshold_hours_callback', 'rt_simple_options', 'rt_simple_date_main_section');
 add_settings_field('display_on_pages', 'Display on Pages', 'display_on_pages_callback', 'rt_simple_options', 'rt_simple_date_main_section');
 add_settings_field('display_on_posts', 'Display on Posts', 'display_on_posts_callback', 'rt_simple_options', 'rt_simple_date_main_section');
 add_settings_field('custom_css', 'Custom CSS', 'custom_css_callback', 'rt_simple_options', 'rt_simple_date_main_section');
 add_settings_field('delete_options_enabled', 'Delete Options on Deactivation', 'delete_options_checkbox_callback', 'rt_simple_options', 'rt_simple_date_main_section');
+add_settings_field('custom_text', 'Custom Text for "Last Updated On"', 'custom_text_callback', 'rt_simple_options', 'rt_simple_date_main_section');
+add_settings_field('display_original_publish_date', 'Display Original Publish Date', 'display_original_publish_date_callback', 'rt_simple_options', 'rt_simple_date_main_section');
 }
-
 
 // ======== BEGIN ADMIN SECTION SETUP AND FORMATTING ========
 //Sets the the menu page options
 function rt_simple_plugin_menu() {
   add_menu_page(
-      'RT Simple Plugin Settings', // Page title
+      'Raymond Tec Simple Plugin Settings', // Page title
       'RT Simple', // Menu title
       'manage_options', // Capability required to access the menu
       'rt-simple-settings', // Menu slug (unique identifier)
@@ -75,20 +80,39 @@ function rt_simple_settings_page() {
 
   // Add your settings form or content here
   ?>
-      <form method="post" action="options.php">
-          <?php settings_fields('rt_simple_options'); ?>
-          <?php do_settings_sections('rt_simple_options'); ?>
-          <input type="submit" class="button-primary" value="Save Changes">
-      </form>
-  <?php  
+  <div class="rt-simple-columns-wrapper">
+      <div class="rt-simple-column">
+          <form method="post" action="options.php">
+              <?php settings_fields('rt_simple_options'); ?>
+              <?php do_settings_sections('rt_simple_options'); ?>
+              <input type="submit" class="button-primary" value="Save Changes">
+          </form>
+      </div>
+      <div class="rt-simple-column">
+          <h3>Additional Settings</h3>
+          <?php
+          // Render only the "Display Original Publish Date" option
+          settings_fields('rt_simple_options');
+          do_settings_fields('rt_simple_options', 'rt_simple_date_main_section');
+          ?>
+      </div>
+  </div>
+  <?php
   echo '</div>';
+  // Add inline styles to create two columns with borders and space
+  echo '<style>
+          .rt-simple-columns-wrapper {
+              display: flex;
+              justify-content: space-between;
+          }
+          .rt-simple-column {
+              flex-basis: 48%; /* Adjust the width as needed */
+              border: 1px solid #ccc; /* Border color and thickness */
+              padding: 10px; /* Add padding inside the columns */
+              margin: 10px; /* Add space between the columns */
+          }
+        </style>';
 }
-
-// ======== DEPRECATED CODE FOR ADDING PLUGIN TO ADMIN->SETTINGS ========
-//Creates the entry in the WP -> Settings menu
-/*add_action('admin_menu', function() {
-  add_options_page('Last Updated Settings', 'Last Updated', 'manage_options', 'rt_simple_options', 'rt_simple_options_page');
-});*/
 
 add_action('admin_menu', 'rt_simple_plugin_menu');
 
@@ -107,11 +131,11 @@ function display_on_posts_callback() {
   echo '<input type="checkbox" id="display_on_posts" name="rt_simple_date_display_options[display_on_posts]" value="1" ' . checked(1, $options['display_on_posts'], false) . ' />';
 }
 
-//Allows setting of the date format
-/*
-function display_style() {
-    $options = get_option('')
-}*/
+// Callback function for displaying original publish date field
+function display_original_publish_date_callback() {
+  $options = get_option('rt_simple_date_display_options');
+  echo '<input type="checkbox" id="display_original_publish_date" name="rt_simple_date_display_options[display_original_publish_date]" value="1" ' . checked(1, $options['display_original_publish_date'], false) . ' />';
+}
 
 //Allows setting of the interval
 function threshold_hours_callback() {
@@ -127,6 +151,14 @@ function custom_css_callback() {
   $options = get_option('rt_simple_date_display_options');
   $custom_css = isset($options['custom_css']) ? $options['custom_css'] : '';
   echo '<textarea id="custom_css" name="rt_simple_date_display_options[custom_css]" rows="4" cols="50">' . esc_textarea($custom_css) . '</textarea>';
+}
+
+// Callback function for the custom text field
+function custom_text_callback() {
+  $options = get_option('rt_simple_date_display_options');
+  $custom_text = isset($options['custom_text']) ? esc_attr($options['custom_text']) : '';
+
+  echo '<input type="text" id="custom_text" name="rt_simple_date_display_options[custom_text]" value="' . $custom_text . '" />';
 }
 
 //Adds the option to delete the plugin's settings upon deletion
@@ -159,18 +191,27 @@ register_deactivation_hook(__FILE__, 'rt_simple_date_deactivation');
 function show_rt_simple_date( $content ) {
   $options = get_option('rt_simple_date_display_options');
   $threshold_hours = isset($options['threshold_hours']) ? intval($options['threshold_hours']) : 12;
-  $custom_css = isset($options['custom_css']) ? wp_kses_post($options['custom_css']) : '';
+  $custom_text = isset($options['custom_text']) ? esc_html($options['custom_text']) : 'Last Updated on ';
+  $display_original_publish_date = isset($options['display_original_publish_date']) ? intval($options['display_original_publish_date']) : 0;
 
-  // Check if the current content being filtered is a single post
-  if (is_single() && $options['display_on_posts'] || is_page() && $options['display_on_pages']) {
+  // Check if the current content being filtered is a single post or page
+  if ((is_single() && $options['display_on_posts']) || (is_page() && $options['display_on_pages'])) {
       $u_time = get_the_time('U');
       $u_modified_time = get_the_modified_time('U');
+      $u_publish_time = get_the_time('U');
       $custom_content = '';
 
       if ($u_modified_time >= $u_time + ($threshold_hours * 3600)) {
           $updated_date = get_the_modified_time('F jS, Y');
           $updated_time = get_the_modified_time('h:i a');
-          $custom_content .= '<div class="last-updated-date" style="'. $custom_css .'">Last Updated on '. $updated_date . ' at '. $updated_time .'</div>';
+          $custom_content .= '<div class="last-updated-date" style="'. esc_attr($options['custom_css']) .'">' . esc_html($custom_text) . ' ' . $updated_date . ' at ' . $updated_time . '</div>';
+      }
+
+      // Display original publish date if the option is checked
+      if ($display_original_publish_date) {
+          $publish_date = get_the_time('F jS, Y', null, null, false);
+          $publish_time = get_the_time('h:i a', null, null, false);
+          $custom_content .= '<div class="original-publish-date" style="'. esc_attr($options['custom_css']) .'">Originally Published on ' . $publish_date . ' at ' . $publish_time . '</div>';
       }
 
       $custom_content .= $content;
